e752a697601fb4ea39d19d07e8ad2faf80f47ab8
Conrad <grosserconrad@gmail.com>
2021-05-05 23:57:54 +0200

    Fixed bug where the action would run in the wrong directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@
 # x would be for showing the commands before they are executed
 set -eux
 
+cd "$GITHUB_ACTION_PATH"
+
 git log --oneline --decorate $INPUT_BASE_REF..$INPUT_HEAD_REF --format=%B -n 1
