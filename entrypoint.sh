1e10e435aa07ab18752a3dba27cb33cfd7176491
Conrad <grosserconrad@gmail.com>
2021-05-16 12:42:07 +0200

    Added output variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@ set -eux
 
 cd "$GITHUB_ACTION_PATH"
 
-git log --oneline --decorate $INPUT_BASE_REF..$INPUT_HEAD_REF --format=%B -n 1
+CONTENT=$(git log --oneline --decorate $INPUT_BASE_REF..$INPUT_HEAD_REF --format=%B -n 1)
+echo ::set-output name=changelog::$CONTENT
